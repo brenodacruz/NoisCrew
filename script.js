d14df64cffbee981484c78113f82b9b8a4e3d179
brenodacruz <brenohenriquecruz22@gmail.com>
2024-05-02 16:10:57 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let topico01 = document.querySelector('.topico01')
+let topico1page = document.querySelector('.topico1')
+
+topico01.addEventListener('click', function(){
+    topico1page.scrollIntoView({behavior: 'smooth'})
+})
+
+let topico02 = document.querySelector('.topico02')
+let topico2page = document.querySelector('.topico2')
+
+topico02.addEventListener('click', function(){
+    topico2page.scrollIntoView({behavior:'smooth'})
+})
+
+let topico03 = document.querySelector('.topico03')
+let topico3page = document.querySelector('.topico3')
+
+topico03.addEventListener('click', function(){
+    topico3page.scrollIntoView({behavior:'smooth'})
+})
